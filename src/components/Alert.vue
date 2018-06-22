60c48a0f09e1e6109ccd0668d19b766a268f7a91
jingjing.zhou1128 <ZJJ_zjj1128@163.com>
2018-06-22 18:00:47 +0800

    alter

---
+++ src/components/Alert.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="alert">
+    <p class="alert-content">{{ content }}</p>
+    <span class="alert-close" @click="close">x</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: ['content'],
+  data(){
+    return {
+      isClose: false
+    }
+  },
+  methods: {
+    close(){
+      // this.isClose = true
+      this.$emit('hidden')
+    }
+  }
+}
+</script>
+
+<style>
+  .alert {
+    background: #f5b0b0;
+    opacity: 0.7;
+    padding: 10px 15px;
+    border-radius: 3px;
+    color: #fff;
+    position: relative;
+  }
+  .alert-close {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+</style>
+
--- src/components/TopicCreate.vue
+++ src/components/TopicCreate.vue
@@ -8,19 +8,21 @@
         </ul>
       </div>
       <div class="panel-body">
-        <div class="form-group form-group-inline">
+        <!-- <p class="error-info" v-if="errorInfo">{{ errorInfo }}</p> -->
+        <alert :content="errorInfo" v-if="errorInfo" @hidden="handleClose"></alert>
+        <div class="form-group form-group-inline" :class="{ 'error': tabIsError }">
           <label>选择板块:</label>
           <select id="tab" name="tab" v-model="tab">
             <option>请选择</option>
-            <option vlaue="share">分享</option>
-            <option vlaue="ask">问答</option>
-            <option vlaue="job">招聘</option>
+            <option value="share">分享</option>
+            <option value="ask">问答</option>
+            <option value="job">招聘</option>
           </select>
         </div>
-        <div class="form-group form-group-block">
+        <div class="form-group form-group-block" :class="{ 'error': titleIsError }">
           <input type="text" name="title" id="title" v-model="title" placeholder="请输入标题">
         </div>
-        <div class="form-group form-group-block">
+        <div class="form-group form-group-block" :class="{ 'error': contentIsError }">
           <textarea type="text" name="content" id="content" v-model="content" rows="20" style="resize: none;"></textarea>
         </div>
         <button class="btn btn-success" @click="publishTopic">提交</button>
@@ -31,13 +33,30 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import Alert from '@/components/Alert'
 export default {
   name: 'TopicCreate',
   data(){
     return {
       tab: '请选择',
       title: '',
-      content: ''
+      content: '',
+      tabIsError: false,
+      titleIsError: false,
+      contentIsError: false,
+      errorInfo: ''
+      // tab: {
+      //   value: '请选择',
+      //   isError: 'false'
+      // },
+      // title: {
+      //   value: '',
+      //   isError: false
+      // },
+      // content: {
+      //   value: '',
+      //   isError: false
+      // }
     }
   },
   computed: {
@@ -47,16 +66,19 @@ export default {
     ])
   },
   methods: {
+    formIsVaild(){
+      let tabs = ['share', 'ask', 'job']
+      let _this = this
+      if (!tabs.find((val) => val=== _this.tab)) _this.tabIsError = true
+      if (_this.title.trim().length < 10) _this.titleIsError = true
+      if (!_this.content.trim()) _this.contentIsError = true
+      if (_this.tabIsError || _this.titleIsError || _this.contentIsError) return false
+      return true
+    },
     publishTopic(){
-      let tab = ''
-      if(this.tab === '分享') {
-        tab = 'share'
-      }else if (this.tab === '问答') {
-        tab = 'ask'
-      }else if(this.tab === '招聘'){
-        tab = 'job'
-      }
-      if(!tab || !this.title.trim() || !this.content.trim()) return false
+      // if(!this.formIsVaild()) return false
+      let tab = this.tab
+      // if(!tab || !this.title.trim() || !this.content.trim()) return false
       this.$http.post('https://www.vue-js.com/api/v1/topics', {
         accesstoken: this.accesstoken.trim(),
         title: this.title.trim(),
@@ -69,12 +91,41 @@ export default {
         }
       }).catch(error => {
         console.log(error)
+        this.errorInfo = "创建话题失败！"
       })
+    },
+    handleClose(){
+      this.errorInfo = ""
     }
-  }
+  },
+  watch: {
+    tab: function(){
+      let tabs = ['share', 'ask', 'job']
+      if (!tabs.find((val) => val === this.tab)){
+        this.tabIsError = true
+      }else {
+        this.tabIsError = false
+      }
+    },
+    title: function(){
+      if (this.title.trim().length < 10){
+        this.titleIsError = true
+      }else {
+        this.titleIsError = false
+      }
+    },
+    content: function(){
+      if (!this.content.trim()){
+        this.contentIsError = true
+      }else {
+        this.contentIsError = false
+      }
+    }
+  },
+  components: { Alert }
 }
 </script>
 
 <style>
-
+  
 </style>
